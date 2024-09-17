668cbf6077fe7dbaae6c7d499161fe0182c528dd
Domingos Canhanga <canhanga96@gmail.com>
2024-09-17 12:14:04 +0100

    feat: config vitest

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { env } from '@/env';
+import { env } from '@/env/config.js';
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient({
---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,4 @@
+import { expect, test } from 'vitest';
+test('it is working', () => {
+	expect(2 + 2).equal(4);
+});
